e431053d98dfab0bba3621763fd33c41210ef424
cosmokhairul <porosh.means.touch@gmail.com>
2022-07-22 23:37:05 +0600

    first commit

---
+++ findOddNumber.js
@@ -0,0 +1,11 @@
+//Finding odd numbers from m to n 
+
+function findOddNumber(m, n) {
+    for (let i = m; i <= n; i += 2) {
+        console.log(i);
+    }
+}
+
+const firstNumber = 50;
+const lastNumber = 80;
+findOddNumber(firstNumber, lastNumber);
---
+++ findSumOfThreeNumbers.js
@@ -0,0 +1,12 @@
+// Find the sum of random three mumbers
+
+function findSumOfThreeNumbers(x, y, z) {
+    const total = x + y + z;
+    return total;
+}
+
+const firstNumber = 10;
+const secondNumber = 20;
+const thirdNumber = 30;
+const output = findSumOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+console.log(output);
